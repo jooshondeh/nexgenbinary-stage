3b6793d4652724357a788318c1e33d67832842f8
jooshondeh <admin@nexgenbinary.com>
2026-02-19 17:37:29 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
     import { defineConfig } from 'astro/config';
 
     export default defineConfig({
-      site: 'https://ORG_OR_USER.github.io',
+      site: 'https://jooshondeh.github.io',
       base: '/nexgenbinary-stage',
 });
